741998e3bf4f086ce52c664544522ec09562a2aa
blu3gu7ar <blu3gui7ar@gmail.com>
2017-05-23 17:22:40 +0800

    Generated

---
+++ server/test/DaoSpec.scala
@@ -0,0 +1,11 @@
+import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
+import play.api.test.PlaySpecification
+
+/**
+  * Created by blu3gui7ar on 2016/10/23.
+  */
+@RunWith(classOf[JUnitRunner])
+class DaoSpec() extends PlaySpecification {
+
+}
